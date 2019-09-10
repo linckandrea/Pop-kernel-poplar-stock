3484711cc07eab14bf4a13043fc9f0efa730deca
linckandrea <linckandrea@outlook.com>
2019-09-10 18:33:13 +0200

    Makefile: rework setup

--- compiler-script.sh
+++ compiler-script.sh
@@ -1,8 +1,8 @@
 export ARCH=arm64
 make O=out clean
 make O=out mrproper
-export PATH=/home/andrea/uber-64-4.9/bin:$PATH
-export CROSS_COMPILE=aarch64-linux-android-
+export PATH=/home/andrea/aarch64-opt-linux-android/bin:$PATH
+export CROSS_COMPILE=aarch64-opt-linux-android-
 export KBUILD_DIFFCONFIG=poplar_dsds_diffconfig
 make Pop-kernel_base_defconfig O=./out
 make O=out -j4

0162deb280c8fc6186244166a0fdb09d5807aaf6
linckandrea <linckandrea@outlook.com>
2019-09-10 13:30:54 +0200

    compiler-script: use dsds config
     * since work even on single sim

--- compiler-script.sh
+++ compiler-script.sh
@@ -3,6 +3,6 @@ make O=out clean
 make O=out mrproper
 export PATH=/home/andrea/uber-64-4.9/bin:$PATH
 export CROSS_COMPILE=aarch64-linux-android-
-export KBUILD_DIFFCONFIG=poplar_diffconfig
+export KBUILD_DIFFCONFIG=poplar_dsds_diffconfig
 make Pop-kernel_base_defconfig O=./out
 make O=out -j4
